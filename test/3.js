0f2638bbf3a2d6ed2b86c90c59733338cca286b5
Matthew Metnetsky <matthew@cowarthill.com>
2013-05-20 20:57:31 -0400

    test/3.js :: test for empty array without open/close

--- test/3.js
+++ test/3.js
@@ -6,6 +6,16 @@ var tap = require('tap')
     , streamify = require('stream-array')
 ;
 
+test('empty array', function(t) {
+    streamify([]).pipe(stringify()).pipe(concat(function(err, res) {
+        t.notOk(err, 'No error');
+        t.type(res, 'Buffer', 'concat results into a Buffer');
+        t.equal('', res.toString(), 'result matches expectation');
+
+        t.end();
+    }));
+});
+
 test('pretty-print empty array', function(t) {
     streamify([]).pipe(stringify({open:'[', close:']'})).pipe(concat(function(err, res) {
         t.notOk(err, 'No error');
