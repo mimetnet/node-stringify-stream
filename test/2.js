6e087148a0ab7ec09366a28410ba552e937301bc
Matthew Metnetsky <matthew@cowarthill.com>
2014-11-09 22:08:17 -0500

    tape :: migrate from tap to tape for browserify, testling, and coverage

--- test/2.js
+++ test/2.js
@@ -1,16 +1,15 @@
-var tap = require('tap')
-    , test = tap.test
-    , stringify = require('..')
-    , concat = require('concat-stream')
+var concat = require('concat-stream')
     , os = require('os')
     , streamify = require('stream-array')
+    , stringify = require('..')
+    , test = require('tape')
 ;
 
 test('newline separated ints', function(t) {
     streamify([1,2,3]).pipe(stringify()).pipe(concat(function(res) {
         var match = [1,2,3].join(os.EOL);
 
-        t.type(res, 'Buffer', 'concat results into a Buffer');
+        t.ok(Buffer.isBuffer(res), 'concat results into a Buffer');
         t.equal(match, res.toString(), 'result matches expectation');
 
         t.end();
@@ -21,7 +20,7 @@ test('array of ints', function(t) {
     var opts = {open:'[', close:']'};
 
     streamify([1,2,3]).pipe(stringify(opts)).pipe(concat(function(res) {
-        t.type(res, 'Buffer', 'concat results into a Buffer');
+        t.ok(Buffer.isBuffer(res), 'concat results into a Buffer');
         t.equal('[1,2,3]', res.toString(), 'result matches expectation');
 
         t.end();
@@ -30,7 +29,7 @@ test('array of ints', function(t) {
 
 test('pretty-print array of objects', function(t) {
     streamify([{one:1}]).pipe(stringify(null, null, '\t')).pipe(concat(function(res) {
-        t.type(res, 'Buffer', 'concat results into a Buffer');
+        t.ok(Buffer.isBuffer(res), 'concat results into a Buffer');
         t.equal('{\n\t"one": 1\n}', res.toString(), 'result matches expectation');
 
         t.end();
